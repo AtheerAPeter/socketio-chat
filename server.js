45ce2fed4d53cb0b312a23235828d43e63b30912
AtheerAPeter <atheer07713991785@gmail.com>
2020-07-08 03:58:25 +0300

    css file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 users = [];
 connections = [];
+app.use(express.static("public"));
 server.listen(process.env.PORT || 3000);
 console.log("server running");
 app.get("/", function (req, res) {
